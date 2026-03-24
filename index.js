0cbd0a90b1a711a950d57e6dc40a8e3bec3e14fd
grace aminga <grace.aminga@student.moringaschool.com>
2026-03-24 13:08:05 +0300

    js-functions

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
 
+function isPalindrome(word) {
+  let cleaned = word.toLowerCase();
+  let reversed = cleaned.split("").reverse().join("");
+  return cleaned === reversed;
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  let discount = (originalPrice * discountPercentage) / 100;
+  return originalPrice - discount;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
